6a744a3b61f1dca6b69b395c95d3064d7a02a15a
zmzhydron <zmzhydron@Outlook.com>
2019-01-29 12:01:56 +0800

    tag test

--- 1.js
+++ 1.js
@@ -4,7 +4,7 @@ for (let value of a.values()) {
   console.log(value, "...............");
 }
 tag 1
-commit 2
+tag222222222222222222222
 let bb = [...a];
 console.log(bb);
 console.log(a.includes(1));
